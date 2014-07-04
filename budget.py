0bca09339bb49e4540c5be8162e11ea3e8106200
Matt Deckard <matthew.d.deckard@gmail.com>
2014-07-04 16:49:47 -0500

    Create a PySide GUI window.

---
+++ budget.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+from PySide import QtGui
+
+app = QtGui.QApplication(sys.argv)
+
+wid = QtGui.QWidget()
+wid.resize(250, 150)
+wid.setWindowTitle('Simple')
+wid.show()
+
+sys.exit(app.exec_())
