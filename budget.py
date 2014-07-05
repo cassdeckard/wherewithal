9f8d557ff99d98587acf1e10843418422dfe6802
Matt Deckard <matthew.d.deckard@gmail.com>
2014-07-05 09:40:33 -0500

    Add stubby data model

--- budget.py
+++ budget.py
@@ -1,7 +1,45 @@
 #!/usr/bin/env python
 
 import sys
-from PySide import QtGui
+from PySide import QtGui, QtCore
+
+class DataModel(QtCore.QAbstractItemModel) :
+   def __init__(self, parent=None) :
+      super(DataModel, self).__init__(parent)
+
+   def columnCount(self, parent) :
+      return 2
+
+   def rowCount(self, parent) :
+      if parent.isValid() :
+         return 0
+      return 3
+
+   def data(self, index, role) :
+      if not index.isValid() :
+         return None
+
+      if role != QtCore.Qt.DisplayRole :
+         return None
+
+      return "data @ {}".format(index)
+
+   def headerData(self, section, orientation, role) :
+      return ("col_1", "col_2")
+
+   def index(self, row, column, parent) :
+      if not self.hasIndex(row, column, parent) :
+         return QtCore.QModelIndex()
+
+      if not parent.isValid() :
+         if column < 2 and row < 3 :
+            return self.createIndex(row, column, "index @({}, {})".format(row, column))
+
+   def parent(self, index) :
+      if not index.isValid() :
+         return QtCore.QModelIndex()
+      return QtCore.QModelIndex()
+
 
 class MainApp(QtGui.QTreeView) :
     def __init__(self) :
@@ -12,6 +50,7 @@ class MainApp(QtGui.QTreeView) :
     def initUI(self) :
         self.setGeometry(300, 300, 250, 150)
         self.setWindowTitle('Budget')
+        self.setModel(DataModel())
 
         self.show()
 
