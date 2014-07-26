f68653fdd8b02466eb015b5415511825364e1d0a
Matt Deckard <matthew.d.deckard@gmail.com>
2014-07-26 07:09:24 -0500

    Add silly failing test

---
+++ test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import unittest
+
+class Test(unittest.TestCase) :
+
+    def setUp(self) :
+        pass
+
+    def tearDown(self) :
+        pass
+
+    def test_test(self) :
+        self.assertTrue(False)
+
+if __name__ == '__main__' :
+    unittest.main()
