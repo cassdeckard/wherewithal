341df3e18ca0289a6b9140d381d1186f10d6ce54
Matt Deckard <matthew.d.deckard@gmail.com>
2014-08-05 06:52:33 -0500

    Use composition instead of inheritance of list

--- Ledger.py
+++ Ledger.py
@@ -1,8 +1,18 @@
 
-class Ledger(list) :
+class Ledger(object) :
     def __init__(self) :
+        self._list = []
         pass
 
     def add_transaction(self, transaction) :
-        self.append({})
+        self._list.append({})
         pass
+
+    def __delitem__(self, key) :
+        self._list.__delitem__(key)
+
+    def __getitem__(self, key) :
+        return self._list.__getitem__(key)
+
+    def __setitem__(self, key) :
+        self._list.__setitem__(key)
