8c174388aefa3907aeb8733bb3d4c77c770eefe7
Matt Deckard <matthew.d.deckard@gmail.com>
2014-08-10 15:23:52 -0500

    Add _parent field with setter/getter

--- DataModelAdapter.py
+++ DataModelAdapter.py
@@ -4,6 +4,7 @@ class DataModelAdapter(object) :
     def __init__(self, data) :
         self._data = data
         self._children = set()
+        self._parent = None
         pass
 
     def numChildren(self) :
@@ -17,3 +18,9 @@ class DataModelAdapter(object) :
 
     def addChild(self, child) :
         self._children.add(child)
+
+    def setParent(self, parent) :
+        self._parent = parent
+
+    def parent(self) :
+        return self._parent
