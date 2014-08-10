78e7442aad307f8bae3f735e35a24dc705fb9681
Matt Deckard <matthew.d.deckard@gmail.com>
2014-08-10 09:17:40 -0500

    SPIKE OUT

--- DataModelAdapter.py
+++ DataModelAdapter.py
@@ -3,13 +3,31 @@ class DataModelAdapter(object) :
 
     def __init__(self, data) :
         self._data = data
+        self._children = set()
+        self._parent = None
         pass
 
     def numChildren(self) :
-        return len(self._data)
+        return len(self._children)
 
     def hasData(self) :
-        return True
+        return self._data is not None
 
     def getData(self, key) :
-        return self._data[key]
+        if key in self._data :
+            return self._data[key]
+        return None
+
+    def addChild(self, child) :
+        child.setParent(self)
+        self._children.add(child)
+
+    def child(self, row) :
+        children_list = [c for c in self._children]
+        return children_list[row]
+
+    def setParent(self, parent) :
+        self._parent = parent
+
+    def parent(self) :
+        return self._parent
--- tests/TestDataModelAdapter.py
+++ tests/TestDataModelAdapter.py
@@ -5,7 +5,7 @@ import unittest
 class TestDataModelAdapter(unittest.TestCase) :
 
     def setUp(self) :
-        self.test_data = {}
+        self.test_data = []
         self.test_object = DataModelAdapter.DataModelAdapter(self.test_data)
 
     def tearDown(self) :
@@ -22,11 +22,13 @@ class TestDataModelAdapter(unittest.TestCase) :
         self.assertTrue(self.test_object.hasData())
 
     def test_numChildren_returns_length_of_data(self) :
-        self.test_data['foo'] = 1
+        self.test_data.append({'foo': 1})
         self.assertEqual(self.test_object.numChildren(), 1)
 
     def test_getData_returns_data_for_given_key(self) :
         key = 'bar'
         expected = 'hello'
-        self.test_data[key] = expected
+        self.test_data.append({key: expected})
         self.assertEqual(self.test_object.getData(key), expected)
+
+    #def test_child_returns
